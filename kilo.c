f88a7ebfc4714c97a19fc660e515f9b7b40bc66f
cuijiawe <cuijiawe@gmail.com>
2023-11-15 16:35:42 +0800

    edit editorDrawRows, add current_color to reduce esc sequences

--- kilo.c
+++ kilo.c
@@ -534,37 +534,48 @@ void editorDrawRows(struct abuf *ab) {
 	for (y = 0; y < E.screenrows; y++) {
 		int filerow = y + E.rowoff;
 		if (filerow >= E.numrows) {
-		if (E.numrows == 0 && y == E.screenrows / 3) {
-  			char welcome[80];
-			int welcomelen = snprintf(welcome, sizeof(welcome),"Kilo editor -- version %s", KILO_VERSION);  
-		 	if (welcomelen > E.screencols) welcomelen = E.screencols;
-			int padding = (E.screencols - welcomelen) / 2;
-			if (padding) {
-				abAppend(ab, "~", 1);
-			 	padding--;
+			if (E.numrows == 0 && y == E.screenrows / 3) {
+  				char welcome[80];
+				int welcomelen = snprintf(welcome,
+										  sizeof(welcome),
+									"Kilo editor -- version %s",
+										  KILO_VERSION);  
+		 		if (welcomelen > E.screencols) 
+					welcomelen = E.screencols;
+				int padding = (E.screencols - welcomelen) / 2;
+				if (padding) {
+					abAppend(ab, "~", 1);
+			 		padding--;
     			}
-			while (padding--) abAppend(ab, " ", 1);
-			abAppend(ab, welcome, welcomelen);
-		} else {
+				while (padding--) abAppend(ab, " ", 1);
+				abAppend(ab, welcome, welcomelen);
+			} else {
 			abAppend(ab, "~", 1);
-		}
+			}
 		} else {
 			int len = E.row[filerow].rsize - E.coloff;
 			if (len < 0) len = 0;
 			if (len > E.screencols) len = E.screencols;
 			char *c = &E.row[filerow].render[E.coloff];
 			unsigned char *hl = &E.row[filerow].hl[E.coloff];
+			int current_color = -1;
 			int j;
 			for (j = 0; j < len; j++) {
 				if (hl[j] == HL_NORMAL) {
-					abAppend(ab, "\x1b[39m", 5);
+					if (current_color != -1) {
+						abAppend(ab, "\x1b[39m", 5);
+						current_color = -1;
+					}
 					abAppend(ab, &c[j], 1);
 				} else {
 					int color = editorSyntaxToColor(hl[j]);
-					char buf[16];
-					int clen = snprintf(buf, sizeof(buf), 
-									"\x1b[%dm", color);
-					abAppend(ab, buf, clen);
+					if (color != current_color) {
+						current_color = color;
+						char buf[16];
+						int clen = snprintf(buf, sizeof(buf), 
+											"\x1b[%dm", color);
+						abAppend(ab, buf, clen);
+					}
 					abAppend(ab, &c[j], 1);
 				}
 			}
