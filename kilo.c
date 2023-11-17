99cdfea333bdde38eb75c27c1f3c66c762056566
cuijiawe <cuijiawe@gmail.com>
2023-11-17 21:36:33 +0800

    add cx cy, reposition cursor after drawRows

--- kilo.c
+++ kilo.c
@@ -17,7 +17,8 @@
 /*** data ***/
 
 struct editorConfig {
-	int screenrows;
+	int cx, cy;       // cursor position
+	int screenrows;   // set by getWindowSize
 	int screencols;
 	struct termios orig_termios;
 };
@@ -134,7 +135,11 @@ void editorRefreshScreen() {
 
 	editorDrawRows(&ab);
 
-	abAppend(&ab, "\x1b[H", 3);
+	char buf[32];
+	snprintf(buf, sizeof(buf), 
+				"\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	abAppend(&ab, buf, strlen(buf));
+
 	abAppend(&ab, "\x1b[?25h", 6); // back show cursor
 
 	write(STDOUT_FILENO, ab.b, ab.len);
@@ -158,6 +163,9 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+	E.cx = 0;
+	E.cy = 0;
+
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) 
 		die("getWindowSize");
 }
